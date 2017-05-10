42d49facae99c81fed85b2493b3797a0174b75c0
olegbilyk777 <olegbilyk777@gmail.com>
2017-05-10 19:45:56 +0100

    temp_fix

--- temp_1.rb
+++ temp_1.rb
@@ -1,12 +1,12 @@
-print "Input temperature and skale (C or F): "
+print 'Input temperature and skale (C or F): '
 
 str = gets
 
-exit if str.nil? or str.empty?
+exit if str.nil? || str.empty?
 
 str.chomp!
 
-temp, scale = str.split (" ")
+temp, scale = str.split(' ')
 
 abort "#{temp} invalid number." if temp !~ /-?\d+/
 
@@ -14,26 +14,26 @@ temp = temp.to_f
 
 case scale
 
-when C 'c'
+when 'C', 'c'
 
-	f = 1.8*temp + 32
+  f = 1.8 * temp + 32
 
-when F, "f"
+when 'F', 'f'
 
-	c = (5.0 / 9.0) * (temp - 32)
+  c = (5.0 / 9.0) * (temp - 32)
 
 else
 
-  abort "You must specify C or F"
+  abort 'You must specify C or F'
 
 end
 
-   if f.nil?
+if f.nil?
 
-     print "#{c} degrees C\n"
+  print "#{c} degrees C\n"
 
-   else
+else
 
-     print "#{f} degrees F\n"
+  print "#{f} degrees F\n"
 
-   end
+end
